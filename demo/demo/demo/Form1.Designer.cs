742fbe010899c4726478d91eb7e04f179caee934
NguyenBaoMy-sangEratosthenes <my.nb.62cntt@ntu.edu.vn>
2022-12-26 09:54:00 +0700

    Sang erotosthenes

---
+++ demo/demo/demo/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eratosthenes = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // eratosthenes
+            // 
+            this.eratosthenes.Location = new System.Drawing.Point(12, 12);
+            this.eratosthenes.Name = "eratosthenes";
+            this.eratosthenes.Size = new System.Drawing.Size(359, 359);
+            this.eratosthenes.TabIndex = 0;
+            this.eratosthenes.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(429, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(359, 359);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 391);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.eratosthenes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox eratosthenes;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ sangEratosthenes/sangEratosthenes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace sangEratosthenes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch tEra = new Stopwatch();
+			/*int n;
+			n = int.Parse(Console.ReadLine());*/
+			string[] inp = File.ReadAllLines("dauVao.txt");
+			for (int f = 0; f < inp.Length; f++)
+			{
+				//Thời gian bắt đầu
+				tEra.Start();
+				//Khởi tạo mảng khởi tạo ban đầu đều là số nguyên tố
+				bool[] a = new bool[int.Parse(inp[f]) + 1];
+				for (int i = 2; i <= int.Parse(inp[f]); i++)
+					a[i] = true;
+
+				//Tìm số nguyên tố
+				for (int i = 2; i < Math.Sqrt(int.Parse(inp[f])); i++)
+				{
+					if (a[i] == true)
+					{
+						int j = (int)2 * i; ;
+						while (j <= int.Parse(inp[f]))
+						{
+							a[j] = false;
+							j = j + i;
+						}
+					}
+				}
+
+				Console.WriteLine(f + ". ");
+
+				//In ra các số nguyên tố
+				/*for (int i = 2; i <= int.Parse(inp[f]); i++)
+					if (a[i] == true)
+						Console.Write("{0} ", i);*/
+
+				// Thời gian kết thúc
+				tEra.Stop();
+
+				//Tổng thời gian thực hiện 
+				string t = string.Format("\nTime: {0}", tEra.ElapsedTicks);
+				File.WriteAllText("dauRa.txt", t);
+
+				//In ra file
+				string o = File.ReadAllText("dauRa.txt");
+				Console.WriteLine(o);
+			}
+			Console.ReadKey();
+		}
+	}
+}

1b1b2432b51d5e1d9b8008f4d22ded59ff9064cf
NguyenBaoMy-sangEratosthenes <my.nb.62cntt@ntu.edu.vn>
2022-12-29 21:38:33 +0700

    ghep chup 2 giai thuat ne

--- sangEratosthenes/sangEratosthenes/Program.cs
+++ sangEratosthenes/sangEratosthenes/Program.cs
@@ -9,14 +9,17 @@ namespace sangEratosthenes
 		static void Main(string[] args)
 		{
 			Stopwatch tEra = new Stopwatch();
+			Stopwatch tSun = new Stopwatch();
 			/*int n;
 			n = int.Parse(Console.ReadLine());*/
 			string[] inp = File.ReadAllLines("dauVao.txt");
+
 			for (int f = 0; f < inp.Length; f++)
 			{
-				//Thời gian bắt đầu
+				//Thời gian bắt đầu thực hiện giải thuật Sàng Eratosthenes
 				tEra.Start();
-				//Khởi tạo mảng khởi tạo ban đầu đều là số nguyên tố
+
+				//Khởi tạo mảng kiểm tra ban đầu đều là số nguyên tố
 				bool[] a = new bool[int.Parse(inp[f]) + 1];
 				for (int i = 2; i <= int.Parse(inp[f]); i++)
 					a[i] = true;
@@ -38,20 +41,51 @@ namespace sangEratosthenes
 				Console.WriteLine(f + ". ");
 
 				//In ra các số nguyên tố
-				/*for (int i = 2; i <= int.Parse(inp[f]); i++)
+				Console.WriteLine("Eratosthenes: ");
+				for (int i = 2; i <= int.Parse(inp[f]); i++)
 					if (a[i] == true)
-						Console.Write("{0} ", i);*/
+						Console.Write("{0} ", i);
 
 				// Thời gian kết thúc
 				tEra.Stop();
 
 				//Tổng thời gian thực hiện 
-				string t = string.Format("\nTime: {0}", tEra.ElapsedTicks);
-				File.WriteAllText("dauRa.txt", t);
+				string t1 = string.Format("\nTime of Eratosthenes: {0} ticks = {1}s", tEra.ElapsedTicks, Math.Round(1.0 * (tEra.ElapsedTicks * 0.00000001), 4));
+				File.WriteAllText("dauRa.txt",t1);
 
 				//In ra file
-				string o = File.ReadAllText("dauRa.txt");
-				Console.WriteLine(o);
+				string o1 = File.ReadAllText("dauRa.txt");
+				Console.WriteLine(o1);
+
+				tSun.Start();
+
+				//Khởi tạo mảng ban đầu đều là số nguyên tố
+				bool[] b = new bool[int.Parse(inp[f]) + 1];
+				for (int i = 1; i <= int.Parse(inp[f]); i++)
+					a[i] = true;
+
+				for (int i = 1; i < (int.Parse(inp[f]) - 2) / 2; i++)
+					for (int j = 2; (i + j + 2 * i * j) <= int.Parse(inp[f]); j++)
+						a[i + j + 2 * i * j] = false;
+
+				Console.WriteLine("Sundaram: ");
+
+				if (int.Parse(inp[f]) > 2)
+					Console.Write(2 + " ");
+
+				for (int i = 1; i < (int.Parse(inp[f]) - 2) / 2; i++)
+					if (a[i] == true)
+						Console.Write(2 * i + 1 + " ");
+
+				tSun.Stop();
+
+				//Tổng thời gian thực hiện 
+				string t2 = string.Format("\nTime of Sundaram: {0} ticks = {1}s", tSun.ElapsedTicks, Math.Round(1.0 * (tSun.ElapsedTicks * 0.00000001), 4));
+				File.WriteAllText("dauRa.txt", t2);
+
+				//In ra file
+				string o2 = File.ReadAllText("dauRa.txt");
+				Console.WriteLine(o2);
 			}
 			Console.ReadKey();
 		}

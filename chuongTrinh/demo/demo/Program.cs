39c75347e875498fa365c173de6dbd4052d4860b
NguyenBaoMy-sangEratosthenes <my.nb.62cntt@ntu.edu.vn>
2023-01-01 14:07:24 +0700

    Hoan thien phan Code

---
+++ chuongTrinh/demo/demo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Stopwatch tEra = new Stopwatch();
+			Stopwatch tSun = new Stopwatch();
+			
+			string[] inp = File.ReadAllLines("dauVao.txt");
+
+			for (int f = 0; f < inp.Length; f++)
+			{
+				tEra.Start();
+
+				bool[] a = new bool[int.Parse(inp[f]) + 1];
+
+				for (int i = 2; i <= int.Parse(inp[f]); i++)
+					a[i] = true;
+
+				for (int i = 2; i < Math.Sqrt(int.Parse(inp[f])); i++)
+				{
+					if (a[i] == true)
+					{
+						int j = (int)2 * i; ;
+						while (j <= int.Parse(inp[f]))
+						{
+							a[j] = false;
+							j = j + i;
+						}
+					}
+				}
+
+				Console.WriteLine(f + ". ");
+
+				Console.WriteLine("Eratosthenes: ");
+
+				for (int i = 2; i <= int.Parse(inp[f]); i++)
+					if (a[i] == true)
+						Console.Write("{0} ", i);
+
+				tEra.Stop();
+
+				string t1 = string.Format("\nTotal time of Eratosthenes: {0} ticks = {1}s", tEra.ElapsedTicks, Math.Round(1.0 * (tEra.ElapsedTicks * Math.Pow(10,-8)), 4));
+				File.AppendAllText("dauRa.txt", t1);
+
+				tSun.Start();
+
+				bool[] b = new bool[int.Parse(inp[f]) + 1];
+
+				for (int i = 1; i <= int.Parse(inp[f]); i++)
+					a[i] = true;
+
+				for (int i = 1; i < (int.Parse(inp[f]) - 2) / 2; i++)
+					for (int j = 2; (i + j + 2 * i * j) <= int.Parse(inp[f]); j++)
+						a[i + j + 2 * i * j] = false;
+
+				Console.WriteLine("\nSundaram: ");
+
+				if (int.Parse(inp[f]) > 2)
+					Console.Write(2 + " ");
+
+				for (int i = 1; i < (int.Parse(inp[f]) - 2) / 2; i++)
+					if (a[i] == true)
+						Console.Write(2 * i + 1 + " ");
+
+				tSun.Stop();
+
+				string t2 = string.Format("\nTotal time of Sundaram: {0} ticks = {1}s", tSun.ElapsedTicks, Math.Round(1.0 * (tSun.ElapsedTicks * Math.Pow(10,-8)), 4));
+				File.AppendAllText("dauRa.txt", t2);
+
+				string oup = File.ReadAllText("dauRa.txt");
+				Console.WriteLine(oup);
+			}
+			Console.ReadKey();
+		}
+    }
+}
---
+++ chuongTrinh/sangEratosthenes/sangEratosthenes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace sangEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tạo mới một đối tượng Stopwatch để đo thời gian thực hiện giải thuật
+            Stopwatch tEra = new Stopwatch();
+            tEra.Start();
+
+            //nhập n số cần kiểm tra từ bàn phím
+            int n = int.Parse(Console.ReadLine());
+
+            //tạo mảng bool a[] lưu giá trị các phần từ
+            bool[] a = new bool[n + 1];
+
+            //mảng a[] gồm những giá trị là số nguyên tố
+            for (int i = 1; i <= n; i++)
+                a[i] = true;
+
+            //kiểm tra điều kiện loại bỏ những số có dạng i + j + 2 * i * j
+            for (int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (a[i] == true)
+                {
+                    int j = (int)2 * i; ;
+                    while (j <= n)
+                    {
+                        a[j] = false;
+                        j = j + i;
+                    }
+                }
+            }
+
+            //in ra dãy các số nguyên tố từ những phần tử có giá trị true của a[i]
+            for (int i = 2; i <= n; i++)
+                if (a[i] == true)
+                    Console.Write("{0} ", i);
+
+            //Dừng việc đo thời gian
+            tEra.Stop();
+
+            //Tổng thời gian thực hiện 
+            Console.WriteLine("\nTotal time of Sundaram: {0} ticks = {1}s", tEra.ElapsedTicks, Math.Round(1.0 * (tEra.ElapsedTicks * Math.Pow(10, -8)), 4));
+
+            Console.ReadKey();
+        }
+    }
+}
